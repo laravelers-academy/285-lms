be582a12326e956821621d289ce7d0ab4f29610b
Homero Raul <h.raulvc@gmail.com>
2021-06-05 21:28:50 -0500

    06 y 07 - Instalación de laravel Santum y Laravel Fortify

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -4,6 +4,7 @@ window._ = require('lodash');
 // AXIOS
 window.axios = require('axios');
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.withCredentials = true;
 
 // VUEJS
 import { createApp } from 'vue'
